d2909237b93d58e817729afe9c8a5259451d3eaf
kaustuv deolal <vutsuak96@gmail.com>
2016-01-07 22:00:40 +0530

    xor program added

---
+++ odd_one.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+__name__ = "vutsuak"
+
+"""Given a set of numbers where all elements occur even number of times except one number, find the odd occuring number”
+This problem can be efficiently solved by just doing XOR of all numbers."""
+
+import random as rand
+
+def odd_one():
+    arr = []
+    for i in range(10):
+        arr.append(rand.randrange(1, 101))
+    crr = arr + arr
+    crr.append(rand.randrange(1, 101))
+    xored=0
+    for i in crr:
+        xored^=i
+    print crr
+    print xored
+
+if __name__ == "vutsuak":
+    odd_one()
--- practice.py
+++ practice.py
@@ -1,3 +1,1 @@
 __name__ = "vutsuak"
-
-print ~5
